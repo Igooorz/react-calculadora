a93f1db35f6c6be118f5bf1ed68bee70f14c8179
Igor Avilez <gonzalez.igor@hotmail.com>
2020-02-06 00:25:46 -0300

    finalizacao das funcionalidades

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -3,23 +3,84 @@ import './Calculadora.css'
 import Botao from '../components/Botao'
 import Display from '../components/Display'
 
+const inicialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculadora extends Component {
+
+    state = {...inicialState}
+
     limpaMemoria = () => {
-        console.log('limpar')
+        this.setState({...inicialState})
     }
 
     setOperation = (op) => {
+        console.log(this.state)
         console.log(op)
+        if (this.state.current === 0) {
+            this.setState({
+                clearDisplay: true,
+                operation: op,
+                current: 1,
+            })
+        } else {
+            let equals = op === '='
+            let currentOperation = this.state.operation
+
+            let values = [...this.state.values]
+            try{
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: String(values[0]),
+                operation: equals ? null : op,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
+        console.log(this.state)
     }
 
     addDigito = (dig) => {
-        console.log(dig)
+        if (dig === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        let clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        let currentValue = clearDisplay ? '' : this.state.displayValue
+        let displayValue = currentValue + dig
+        this.setState({
+            displayValue,
+            clearDisplay: false
+        })
+
+        if (dig !== '.') {
+            let i = this.state.current
+            let newValue = parseFloat(displayValue)
+            let values = [...this.state.values]
+            values[i] = newValue
+            this.setState({
+                values
+            })
+            console.log(values)
+        }
     }
     
     render() {
         return (
             <div className="calculadora">
-                <Display value={0} />
+                <Display value={this.state.displayValue} />
                 <Botao value="AC" click={this.limpaMemoria} triple/>
                 <Botao value="/" click={this.setOperation} operation/>
                 <Botao value="7" click={this.addDigito}/>
